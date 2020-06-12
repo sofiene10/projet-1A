77705f54a3a530f22b26f1b7bd31ee7438e9570c
sofienemazlout <sofiene.mazlout@esprit.tn>
2020-06-12 10:22:52 +0100

    scroll

--- sofiene/scroll.c
+++ sofiene/scroll.c
@@ -18,27 +18,27 @@ SDL_Rect camera_pos;
 camera_pos.x=0;
 camera_pos.y=0;
  
-  sprite= IMG_Load ("back.jpg");//ton décors, par ex: 1280x960
+  sprite= IMG_Load ("back.jpg");
  
-   Uint32 x=-100, y=-100; //les variables UINT32
+   Uint32 x=-100, y=-100; 
  
-    //les coordonnées de ton décors sur l'écran, le perso sera affiché au centre de l'écran
+    
  
    SDL_Rect decors={x,y, sprite->w, sprite->h};
  
-   personnage=IMG_Load("joueur1.png");//ton personnage
+   personnage=IMG_Load("joueur1.png");
  
-    //les coordonnées de ton perso sur l'écran, le perso sera affiché au centre de l'écran
+    
  
    SDL_Rect perso={ ecran->w/2 - personnage->w/2, ecran->h/2 - personnage->w/2, personnage->w, personnage->h};
  
    for( SDL_Event evenement; evenement.type!=SDL_QUIT&& evenement.key.keysym.sym!=SDLK_ESCAPE; SDL_PollEvent(&evenement)){
  
-    //remplissage de l'écran d'une couleur ici noir avant d'afficher les images
+   
  
     SDL_FillRect(ecran, NULL, SDL_MapRGB( ecran->format, 0,0,0));
  
-    //Tu dois gérer les événements ici avec switch pour déplacer le background
+    
  
     switch( evenement.type){
  
@@ -62,11 +62,10 @@ camera_pos.y=0;
  
    }
  
-   //tu affectes les nouvelles valeurs de x et y ici pour forcer le background a "scroller" dans toutes les directions
+   
  
     decors.x=x; decors.y=y;
  
-   //tu blittes ici
  
     SDL_BlitSurface( sprite, NULL, ecran, &decors);
  
